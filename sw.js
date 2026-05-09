2974503bbe08ee0a47be14aefae0c1c1d3bae0b0
M Ahmad <278949455+mahmad1223@users.noreply.github.com>
2026-05-09 19:55:30 +0500

    Update service worker for cache version and simplify logic

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'flashcardz-v10';
+const CACHE_NAME = 'flashcardz-v11';
 const STATIC_ASSETS = [
   './',
   './index.html',
@@ -11,42 +11,15 @@ const STATIC_ASSETS = [
   './tailwind.min.js'
 ];
 
-self.addEventListener('install', (event) => {
-  self.skipWaiting(); 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW] Caching all local assets...');
-      return cache.addAll(STATIC_ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    }).then(() => self.clients.claim())
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      // 1. Agar cache mein hai toh wahi de do (Fastest!)
-      if (cachedResponse) return cachedResponse;
-
-      // 2. Nahi hai toh network se lo aur save karo
-      return fetch(event.request).then((networkResponse) => {
-        if (networkResponse.ok && event.request.url.startsWith('http')) {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          });
-        }
-        return networkResponse;
-      });
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
